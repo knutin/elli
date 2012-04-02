45838eae824c389342e0ed56fbce68ab60ce8712
Knut Nesheim <knutin@gmail.com>
2012-04-02 23:34:24 +0200

    Documented elli_middleware.erl

--- src/elli_middleware.erl
+++ src/elli_middleware.erl
@@ -1,7 +1,40 @@
+%% @doc: HTTP request processing middleware.
+%%
+%% This module offers both pre- processing of requests and
+%% post-processing of responses. It can also be used to allow multiple
+%% handlers, where the first handler to return a response
+%% short-circuits the request. It is implemented as a plain elli
+%% handler.
+%%
+%% Usage:
+%%  Config = [
+%%            {mods, [
+%%                    {elli_example_middleware, []},
+%%                    {elli_middleware_compress, []},
+%%                    {elli_example_callback, []}
+%%                   ]}
+%%             ],
+%%  elli:start_link([..., {callback, elli_middleware}, {callback_args, Config}]).
+%%
+%% The configured modules may implement the elli behaviour, in which
+%% case all the callbacks will be used as normal. If handle/2 returns
+%% 'ignore', elli will continue on to the next callback in the list.
+%%
+%% Pre-processing and post-processing is implemented in preprocess/2
+%% and postprocess/3. preprocess/2 is called for each middleware in
+%% the order specified, while postprocess/3 is called in the reverse
+%% order. TODO: Even if a middleware short-circuits the request, all
+%% postprocess middlewares will be called.
+%%
+%% elli_middleware does not add any significant overhead.
+
 -module(elli_middleware).
 -behaviour(elli_handler).
 -export([handle/2, handle_event/3]).
 
+%%
+%% ELLI CALLBACKS
+%%
 
 handle(CleanReq, Config) ->
     Mods = mods(Config),
@@ -13,10 +46,10 @@ handle(CleanReq, Config) ->
 
 handle_event(elli_startup, Args, Config) ->
     lists:foreach(fun code:ensure_loaded/1, lists:map(fun ({M, _}) -> M end,
-                                                      mods(Config))),
+                                                  mods(Config))),
     forward_event(elli_startup, Args, mods(Config));
 handle_event(Event, Args, Config) ->
-    forward_event(Event, Args, Config).
+    forward_event(Event, Args, mods(Config)).
 
 
 
