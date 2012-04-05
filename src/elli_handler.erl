1fcb03675d579cdadb0414174ea70f9100502c46
mhaemmerle <marc.haemmerle@wooga.net>
2012-04-05 17:27:52 +0200

    - define behaviour functions the R14 way for now

--- src/elli_handler.erl
+++ src/elli_handler.erl
@@ -1,18 +1,8 @@
 -module(elli_handler).
 -include("elli.hrl").
 
--callback handle(Request :: #req{}, callback_args()) ->
-    {ok, headers(), body()} |
-    {response_code(), headers(), body()} |
-    {chunk, headers()} |
-    no_return().
+-export([behaviour_info/1]).
 
--type request_event() :: request_complete
-                       | request_throw
-                       | request_exit
-                       | request_error
-                       | request_parse_error
-                       | client_closed
-                       | client_timeout.
-
--callback handle_event(request_event(), list(), callback_args()) -> ok.
+behaviour_info(callbacks) ->
+    [{handle, 2},
+     {handle_event, 3}].
