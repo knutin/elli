knutin/elli

commit ee6afb4336b645916273a17b83c0bdf4d11c81d7

Move keep alive loop logic into accept/3.

src/elli_http.erl
@@ -26,7 +26,14 @@ accept(Server, ListenSocket, Callback) ->
         {ok, Socket} ->
             t(accepted),
             gen_server:cast(Server, accepted),
-            ?MODULE:handle_request(Socket, Callback);
+
+            case ?MODULE:handle_request(Socket, Callback) of
+                keep_alive ->
+                    ?MODULE:handle_request(Socket, Callback);
+                close ->
+                    gen_tcp:close(Socket),
+                    ok
+            end;
         {error, timeout} ->
             ?MODULE:accept(Server, ListenSocket, Callback);
         {error, econnaborted} ->
@@ -53,11 +60,9 @@ handle_request(Socket, {Mod, Args} = Callback) ->
         {response, ResponseCode, UserHeaders, UserBody} ->
             t(user_end),
 
-            ResponseHeaders = [
-                               connection(Req, UserHeaders),
+            ResponseHeaders = [connection(Req, UserHeaders),
                                content_length(UserHeaders, UserBody)
-                               | UserHeaders
-                              ],
+                               | UserHeaders],
             send_response(Socket, Method, ResponseCode, ResponseHeaders, UserBody, Callback),
 
             t(request_end),
@@ -65,13 +70,7 @@ handle_request(Socket, {Mod, Args} = Callback) ->
                              [Req, ResponseCode, ResponseHeaders, UserBody, get_timings()],
                              Args),
 
-            case close_or_keepalive(Req, UserHeaders) of
-                keep_alive ->
-                    ?MODULE:handle_request(Socket, Callback);
-                close ->
-                    gen_tcp:close(Socket),
-                    ok
-            end;
+            close_or_keepalive(Req, UserHeaders);
 
         {chunk, UserHeaders, Initial} ->
             t(user_end),
@@ -91,7 +90,7 @@ handle_request(Socket, {Mod, Args} = Callback) ->
             Mod:handle_event(chunk_complete,
                              [Req, 200, ResponseHeaders, ClosingEnd, get_timings()],
                              Args),
-            ok;
+            close;
 
         {file, ResponseCode, UserHeaders, Filename} ->
             t(user_end),
@@ -106,13 +105,7 @@ handle_request(Socket, {Mod, Args} = Callback) ->
                              [Req, ResponseCode, ResponseHeaders, <<>>, get_timings()],
                              Args),
 
-            case close_or_keepalive(Req, UserHeaders) of
-                keep_alive ->
-                    ?MODULE:handle_request(Socket, Callback);
-                close ->
-                    gen_tcp:close(Socket),
-                    ok
-            end
+            close_or_keepalive(Req, UserHeaders)
     end.
 
 -spec mk_req(Method::http_method(), RawPath::binary(), RequestHeaders::headers(),

commit aa4dd1c92ab90f2acc79ec4f8192a1563eaf2842

Fix broken keep-alive introduced in ee6afb43. Closes #32.

src/elli_http.erl
@@ -7,7 +7,7 @@
 -include("../include/elli.hrl").
 
 -export([start_link/3, accept/3, handle_request/2, chunk_loop/1,
-         split_args/1, parse_path/1]).
+         split_args/1, parse_path/1, keepalive_loop/3]).
 
 -export([mk_req/7]). %% useful when testing.
 
@@ -21,19 +21,12 @@ start_link(Server, ListenSocket, Callback) ->
 %% request, then loops if we're using keep alive or chunked
 %% transfer. If accept doesn't give us a socket within 10 seconds, we
 %% loop to allow code upgrades.
-accept(Server, ListenSocket, Callback) ->
+accept(Server, ListenSocket, {Mod, Args} = Callback) ->
     case catch gen_tcp:accept(ListenSocket, 10000) of
         {ok, Socket} ->
             t(accepted),
             gen_server:cast(Server, accepted),
-
-            case ?MODULE:handle_request(Socket, Callback) of
-                keep_alive ->
-                    ?MODULE:handle_request(Socket, Callback);
-                close ->
-                    gen_tcp:close(Socket),
-                    ok
-            end;
+            ?MODULE:keepalive_loop(Socket, 0, Callback);
         {error, timeout} ->
             ?MODULE:accept(Server, ListenSocket, Callback);
         {error, econnaborted} ->
@@ -42,6 +35,17 @@ accept(Server, ListenSocket, Callback) ->
             ok;
         {error, Other} ->
             exit({error, Other})
+    end.
+
+
+%% @doc: Handle multiple requests on the same connection
+keepalive_loop(Socket, NumRequests, Callback) ->
+    case ?MODULE:handle_request(Socket, Callback) of
+        keep_alive ->
+            ?MODULE:keepalive_loop(Socket, NumRequests, Callback);
+        close ->
+            gen_tcp:close(Socket),
+            ok
     end.
 
 -spec handle_request(port(), callback()) -> ok.