beca39e82f8f1cfca427f08676cae9e9cd524c0e
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2013-06-27 18:22:19 +0200

    Use proc_lib:spawn instead of spawn for more info in crash report. Info about the creator of this processes, sibling processes and more info regarding the crash will be displayed. Note: This does not affect crases in the user code, these are catched by elli. For more info on proc_lib, look here: http://www.erlang.org/doc/man/proc_lib.html

--- src/elli_http.erl
+++ src/elli_http.erl
@@ -20,7 +20,7 @@
 
 -spec start_link(pid(), port(), proplists:proplist(), callback()) -> pid().
 start_link(Server, ListenSocket, Options, Callback) ->
-    spawn_link(?MODULE, accept, [Server, ListenSocket, Options, Callback]).
+    proc_lib:spawn_link(?MODULE, accept, [Server, ListenSocket, Options, Callback]).
 
 -spec accept(pid(), port(), proplists:proplist(), callback()) -> ok.
 %% @doc: Accept on the socket until a client connects. Handles the
